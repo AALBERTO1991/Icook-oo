2812c471a3bbe06f35088f4a908b60ef026a7ae2
Anderson Silva <aalberto1991@outlook.com>
2024-05-30 18:39:20 -0300

    programa atualizado

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Anderson', 10)
-restaurante_praca.receber_avaliacao('Tamara', 8)
-restaurante_praca.receber_avaliacao('Arthur', 5)
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+bebida_suco.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-  Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
-  main()
+    main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     '''Essa classe representa um restaurante e suas caracteristicas'''
@@ -11,35 +12,36 @@ class Restaurante:
     - categoria (str): A categoria do Restaurante
     '''
     def __init__(self, nome, categoria):
-      self._nome = nome.title()
-      self._categoria = categoria.upper()
-      self._ativo = False
-      self._avaliacao = []
-      Restaurante.resturantes.append(self)
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacao = []
+        self._cardapio = []
+        Restaurante.resturantes.append(self)
     
     '''Essa função retorna em string uma representação do restaurante cadastrado'''
     def __str__(self) -> str:
-      return f'{self._nome} | {self._categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     '''Essa função classmethod Exibe uma lista formatada de todos os restaurantes cadastrados
     o parametro .ljust(25) foi utilizado para carrigir a visão do código no terminal
     '''
     @classmethod
     def listar_restaurantes(cls):
-      print (f'\n{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | Status')
-      print('-------------------------------------------------------------------------------------')
-      for restaurante in cls.resturantes:
-        print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)}| {restaurante.ativo}')
-      print()
+        print (f'\n{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | Status')
+        print('-------------------------------------------------------------------------------------')
+        for restaurante in cls.resturantes:
+          print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)}| {restaurante.ativo}')
+        print()
 
     '''Retorna um símbolo indicando o estado de atividade do restaurante.'''
     @property
     def ativo(self):
-      return '✔️' if self._ativo else '❌'
+        return '✔️' if self._ativo else '❌'
     
     '''Alterna o estado de atividade do restaurante.'''
     def alternar_estado(self):
-      self._ativo = not self._ativo
+        self._ativo = not self._ativo
     
     '''
     Registra uma avaliação para o restaurante.
@@ -49,16 +51,31 @@ class Restaurante:
     - nota (float): A nota atribuída ao restaurante (entre 1 e 5).
     '''
     def receber_avaliacao(self, cliente, nota):
-      if 0 < nota <= 5:
-        avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
     
     '''Calcula e retorna a média das avaliações do restaurante.'''
     @property
     def media_avaliacoes(self):
-      if not self._avaliacao:
-        return '-'
-      soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
-      quantidade_de_notas = len(self._avaliacao)
-      media = round(soma_das_notas / quantidade_de_notas, 1)
-      return media
+        if not self._avaliacao:
+          return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
+    
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+
+    @property
+    def exibir_cardapio(self):
+        print(f'Cardapio do Restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1):
+            if hasattr(item, 'descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: {item._preco} | Descrição: {item.descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: {item._preco} | Tamanho: {item.tamanho}'
+                print(mensagem_bebida)
